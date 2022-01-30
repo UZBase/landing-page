09ada77ed9f0c0af48fcb4449727e2ac2e9a3fb4
sardormirzakomilov <mirzakomilovs@gmail.com>
2022-01-30 14:34:17 +0500

    preim_rabota end

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,20 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".carousel-control-prev-icon",
   },
 });
+
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  grabCursor: true,
+  // slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
